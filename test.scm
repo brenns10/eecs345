5a56b14b686dfec6752f2ec93bf820d96b5c7ffa
Stephen Brennan <smb196@case.edu>
2015-02-25 21:36:36 -0500

    Updated test runner to have test groups (by folder).

--- test.scm
+++ test.scm
@@ -1,14 +1,12 @@
 (load "interpreter.scm")
 
-(define pass-tests '(1 2 3 4 5 6 7 8 9 10 15 16 17 18 19 20 21 22 23 24))
-(define pass-vals '(150 -4 10 16 220 5 6 10 5 -39 true 100 false true
-                        30 11 1106 12 16 72))
-(define fail-tests '(11 12 13 14))
-
+(define pass-tests car)
+(define pass-vals cadr)
+(define fail-tests caddr)
 
 (define test-name
-  (lambda (number)
-    (string-append "tests/"
+  (lambda (group number)
+    (string-append group "/"
                    (let ((n (number->string number)))
                      (if (= 2 (string-length n))
                          n
@@ -16,30 +14,33 @@
                    ".txt")))
 
 (define run-test
-  (lambda (number)
-    (interpret (test-name number))))
+  (lambda (group number)
+    (interpret (test-name group number))))
 
 (define run-pass-tests
-  (lambda (tests values)
+  (lambda (group tests values)
     (if (null? tests)
         #t
-        (let ((rv (run-test (car tests))))
+        (let ((rv (run-test group (car tests))))
           (if (equal? rv (car values))
-              (run-pass-tests (cdr tests) (cdr values))
-              (error (string-append "test " (test-name (car tests)) " failed")))))))
+              (run-pass-tests group (cdr tests) (cdr values))
+              (error (string-append "test " (test-name group (car tests)) " failed")))))))
 
 (define run-fail-tests
-  (lambda (tests)
+  (lambda (group tests)
     (if (null? tests)
         #t
         (begin
-          (if (condition? (ignore-errors (lambda () (run-test (car tests)))))
-              (run-fail-tests (cdr tests))
-              (error (string-append "test " (test-name (car tests)) " failed")))))))
+          (if (condition? (ignore-errors (lambda () (run-test group (car tests)))))
+              (run-fail-tests group (cdr tests))
+              (error (string-append "test " (test-name group (car tests)) " failed")))))))
 
-(write-string "Non-error tests: ")
-(display (run-pass-tests pass-tests pass-vals))
-(newline)
-(write-string "Error tests: ")
-(display (run-fail-tests fail-tests))
-(newline)
+(define test
+  (lambda (group)
+    (let ((spec (load (string-append group "/spec.scm"))))
+      (write-string "Non-error tests: ")
+      (display (run-pass-tests group (pass-tests spec) (pass-vals spec)))
+      (newline)
+      (write-string "Error tests: ")
+      (display (run-fail-tests group (fail-tests spec)))
+      (newline))))
---
+++ tests_pt1/spec.scm
@@ -0,0 +1,8 @@
+'(
+  ;; Tests that pass
+  (1 2 3 4 5 6 7 8 9 10 15 16 17 18 19 20 21 22 23 24)
+  ;; Return values for the tests that pass
+  (150 -4 10 16 220 5 6 10 5 -39 true 100 false true 30 11 1106 12 16 72)
+  ;; Tests that fail
+  (11 12 13 14)
+)
