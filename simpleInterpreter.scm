d53546ec17fbc83c98f1dadf736672f4ace62d0c
Stephen Brennan <smb196@case.edu>
2015-04-09 20:53:47 -0400

    Fixed the test runner so we can run our old and new tests.

---
+++ simpleInterpreter.scm
@@ -0,0 +1,8 @@
+;; The old interpreter loop, so we can run our old tests!
+;; Because regression testing is important!
+(define interpret
+  (lambda (filename)
+    (let ((err (lambda (v) (error "You can't break or continue here."))))
+      (return_val (call/cc
+                   (lambda (return)
+                     (Mstate (parser filename) (state-new) return err err)))))))
