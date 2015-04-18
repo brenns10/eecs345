f482f20b155263e97ad64615a9651a9c35bbd45e
Stephen Brennan <smb196@case.edu>
2015-04-18 10:25:47 -0400

    Created a context abstraction.

--- functionInterpreter.scm
+++ functionInterpreter.scm
@@ -1,7 +1,7 @@
 (define interpret
   (lambda (filename)
     (return_val (let* ((err (lambda (v) (error "Can't return/break/continue here.")))
-                       (state (Mstate (parser filename) (state-new) err err err)))
+                       (state (Mstate (parser filename) (state-new) (ctx-new err err err))))
                   (call/cc
                    (lambda (return)
-                     (Mvalue '(funcall main) state return err err)))))))
+                     (Mvalue '(funcall main) state (ctx-new return err err))))))))
